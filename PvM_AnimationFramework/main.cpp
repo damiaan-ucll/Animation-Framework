834817095a52edced6fbc942fa9627afb0fad0e4
Devian <info@devian.tk>
2015-04-21 12:27:43 +0200

    Initial Commit

---
+++ PvM_AnimationFramework/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PvM_AnimationFramework
+//
+//  Created by Damiaan Dufaux on 21/04/15.
+//  Copyright (c) 2015 Damiaan Dufaux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
