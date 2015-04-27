cedfd12f4c0c60080c68fd49a7826708c6b30363
Devian <info@devian.tk>
2015-04-27 13:34:09 +0200

    Excercise 2 - d

--- PvM_AnimationFramework/src/renderer/Renderer.cpp
+++ PvM_AnimationFramework/src/renderer/Renderer.cpp
@@ -8,10 +8,12 @@
 #include "../renderer/Renderer.h"
 
 void Renderer::render(){
-	glColor3f(1,0,0);
+	glColor3f(0,0.4
+			  ,1);
 	glBegin(GL_POLYGON);
-		glVertex2f(50,100);
-		glVertex2f(450,100);
-		glVertex2f(250,400);
+		glVertex2f(700,50);
+		glVertex2f(900,50);
+		glVertex2f(900,450);
+		glVertex2f(700,450);
 	glEnd();
 }
